873702133e2af0da40aee950054cc2f936d0758d
katie stout <katiestout@katies-MacBook-Pro.local>
2018-07-14 20:32:37 -0400

    mongo

--- server.js
+++ server.js
@@ -12,6 +12,13 @@ const app = express();
 const PORT = process.env.PORT || 8080;
 //const db = require('./models');
 
+// If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
+// Set mongoose to leverage built in JavaScript ES6 Promises
+// Connect to the Mongo DB
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI);
 
 
 app.listen(PORT, () => {
